d1f279a72f22299e55bc4b1cbb2b65ff832e56a0
liudmylaberezina <liudmylaberezina@gmail.com>
2023-03-05 22:42:56 +0000

    new changes

---
+++ src/components/layout/Info.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Info() {
+  return (
+
+    <div className="hero bg-base-50">
+    <div className="hero-content flex-col lg:flex-row-reverse">
+          <div>
+        <h1 className="text-2xl font-bold">If you're looking for an interesting site to find profiles on GitHub, you might want to check out GitHub's search functionality on my website, which allows you to search for users, repositories, and code</h1>
+        <p style={{ fontSize: '18px', fontFamily: 'Shantell Sans, cursive' }} className=" text-1xl pt-6  text-{#191A3F} ">Just start typing username</p>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Info
+
